a2edb9ea310162caaec0a1695592a41d73d892f9
Xu_labComputer <915808038@qq.com>
2022-03-03 22:06:45 +0800

    过滤敏感词

---
+++ src/test/java/com/hutb/community/sensitiveTest.java
@@ -0,0 +1,29 @@
+package com.hutb.community;
+
+import com.hutb.community.util.SensitiveFilter;
+import org.apache.catalina.filters.ExpiresFilter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class sensitiveTest {
+
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitive(){
+        String text = "☆赌博，吸..毒。。drug...读书真好。";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+
+    @Test
+    public void test(){
+
+    }
+}
